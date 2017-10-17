d1ff46e8b9b0603c154e24c0a78bb53af35e6600
DESKTOP-161072Q\Mat <mateusz.kuzmik1@gmail.com>
2017-10-17 17:22:39 +0200

    renamed

--- lab2/bilinear.m
+++ lab2/bilinear.m
@@ -4,15 +4,15 @@ close all;
 clc;
 
 %% read img
-original_img = imread('parrot.bmp');
+original_img = imread('rescaled.bmp');
 
 %% show img
 figure(1);
 imshow(original_img);
 
 %% define scales
-xReScale = 3;
-yReScale = 3;
+xReScale = 5;
+yReScale = 5;
 
 %% compute new res
 [actualHeight, actualWidth] = size(original_img);
@@ -38,17 +38,19 @@ for x = 0:newWidth-1
             yN = actualHeight-2;
         end;
         
-        A = double(original_img(xN+1,yN+1));
-        B = double(original_img(xN+1,yN+2));
-        C = double(original_img(xN+2,yN+1));
-        D = double(original_img(xN+2,yN+2));
+        A = double(original_img(yN+1,xN+1));
+        B = double(original_img(yN+1,xN+2));
+        C = double(original_img(yN+2,xN+1));
+        D = double(original_img(yN+2,xN+2));
         
+        i = xN/x;
+        j = yN/y;
          
         rescaledImage(y + 1,x + 1) = ...
-            [1-x, x] * ...
+            [1-i, i] * ...
             [A, D; ...
              B, C] * ...
-            [1-y; y];
+            [1-j; j];
     end
 end
 
---
+++ lab2/nearest_neighbour.m
@@ -0,0 +1,48 @@
+%% clear
+clearvars;
+close all;
+clc;
+
+%% read img
+original_img = imread('chessboard.bmp');
+
+%% show img
+figure(1);
+imshow(original_img);
+
+%% define scales
+xReScale = 5;
+yReScale = 5;
+
+%% compute new res
+[actualHeight, actualWidth] = size(original_img);
+newHeight = floor(yReScale * actualHeight);
+newWidth = floor(xReScale * actualWidth);
+
+%% iterate through image
+xStep = actualWidth / newWidth;
+yStep = actualHeight / newHeight;
+
+rescaledImg = uint8(zeros(newHeight, newWidth));
+
+for x = 0:newWidth-1
+    for y = 0:newHeight-1
+        
+        xNeighbour = floor(x * xStep);
+        yNeighbour = floor(y * yStep);
+        
+        if (xNeighbour > actualWidth) 
+            xNeighbour = actualWidth;
+        end;
+        if (yNeighbour > actualHeight) 
+            yNeighbour = actualHeight;
+        end;
+       
+        rescaledImg(y + 1,x + 1) = original_img(yNeighbour + 1, xNeighbour + 1);
+    end
+end
+
+%% show rescaled Img
+figure(2);
+imshow(rescaledImg);
+imwrite(rescaledImg, 'rescaled.bmp');
