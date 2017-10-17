d602b65cce07159d0fd851b25eb5a20d85ddd6d4
DESKTOP-161072Q\Mat <mateusz.kuzmik1@gmail.com>
2017-10-17 16:35:51 +0200

    imprv

--- lab2/scr2.m
+++ lab2/scr2.m
@@ -11,8 +11,8 @@ figure(1);
 imshow(original_img);
 
 %% define scales
-xReScale = 3;
-yReScale = 3;
+xReScale = 30;
+yReScale = 30;
 
 %% compute new res
 [actualHeight, actualWidth] = size(original_img);
@@ -43,12 +43,14 @@ for x = 0:newWidth-1
         C = double(original_img(yN+2,xN+1));
         D = double(original_img(yN+2,xN+2));
         
+        i = xN/x;
+        j = yN/y;
          
         rescaledImage(y + 1,x + 1) = ...
-            [1-x, x] * ...
+            [1-i, i] * ...
             [A, D; ...
              B, C] * ...
-            [1-y; y];
+            [1-j; j];
     end
 end
 
