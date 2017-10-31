d70e474eb71f14fcb61e732c0f5100a1301459c3
lsriw <lsriw@lsriw.lsriw>
2017-10-31 18:32:31 +0000

    lab 5 zad 1 finished

---
+++ lab5/zad1.m
@@ -0,0 +1,100 @@
+%% clear
+clc;
+close all;
+clearvars;
+
+%% read img
+img = imread('coins.png');
+
+%% show img
+figure;
+subplot(1,2,1);
+imshow(img);
+subplot(1,2,2);
+imhist(img);
+
+%% binarize
+threshold = 85;
+bw_img = im2bw(img, threshold/255);
+
+figure;
+imshow(bw_img);
+
+%% 4
+img = imread('figura.png');
+figure;
+subplot(4,2,1);
+imshow(img);
+subplot(4,2,2);
+imhist(img);
+
+img = imread('figura2.png');
+subplot(4,2,3);
+imshow(img);
+subplot(4,2,4);
+imhist(img);
+
+img = imread('figura3.png');
+subplot(4,2,5);
+imshow(img);
+subplot(4,2,6);
+imhist(img);
+
+img = imread('figura4.png');
+subplot(4,2,7);
+imshow(img);
+subplot(4,2,8);
+imhist(img);
+
+%% 5,6,7
+img = imread('coins.png');
+
+figure;
+subplot(1,6,1);
+
+imshow(img);
+
+subplot(1,6,2);
+imhist(img);
+
+threshold = graythresh(img);
+subplot(1,6,3);
+imshow(im2bw(img, threshold));
+
+threshold = clusterKittler(img);
+subplot(1,6,4);
+imshow(im2bw(img, threshold/255));
+
+threshold = entropyYen(img);
+subplot(1,6,5);
+imshow(im2bw(img, threshold/255));
+
+subplot(1,6,6);
+imshow(im2bw(img, 85/255));
+
+
+%% 8
+img = imread('rice.png');
+
+figure;
+subplot(1,6,1);
+
+imshow(img);
+
+subplot(1,6,2);
+imhist(img);
+
+threshold = graythresh(img);
+subplot(1,6,3);
+imshow(im2bw(img, threshold));
+
+threshold = clusterKittler(img);
+subplot(1,6,4);
+imshow(im2bw(img, threshold/255));
+
+threshold = entropyYen(img);
+subplot(1,6,5);
+imshow(im2bw(img, threshold/255));
+
+subplot(1,6,6);
+imshow(im2bw(img, 120/255));
