79d86421a839388b24e3e3157fc7b6463b807659
DESKTOP-161072Q\Mat <mateusz.kuzmik1@gmail.com>
2017-12-19 14:21:01 +0100

    lab 10 finished

--- lab10/split.m
+++ lab10/split.m
@@ -1,17 +1,26 @@
-function segmented = split(img,x1,y1,x2,y2)
+function split(img,x1,y1,x2,y2)
+
     global minimum;
     global threshold;
-
-    iVec = img(:);
+    global index;
+    global segRes;
+    global MRes;
     
-    mean_ = mean(iVec);
-    std_ = std(iVec);
+    img_part = img(y1:y2,x1:x2);
+    img_mean = mean(img_part(:));
+    img_std = std(img_part(:));
     
-    if (std_ < threshold || (x1-x2) < minimum)
-        segmented = reshape;
-        return
+    if (img_std < threshold) || ((x2-x1) <= minimum)
+        index = index + 1;
+        segRes(y1:y2,x1:x2) = index;
+        MRes(y1:y2,x1:x2) = img_mean; 
+    else
+        new_x = floor((x2+x1)/2);
+        new_y = floor((y2+y1)/2);
+        split(img,x1,y1,new_x,new_y);
+        split(img,new_x+1,y1,x2,new_y);
+        split(img,new_x+1,new_y+1,x2,y2);
+        split(img,x1,new_y+1,new_x,y2);
     end
-    
-    
 end
 
